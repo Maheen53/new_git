51e4fc329f0f163b985eeaa36420dca6786e6bfd
“[firstname <maheenimtiaz536@gmail.com>
2024-04-08 10:39:13 -0700

    my first list

---
+++ main.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let todos = [];
+let conditition = "true";
+while (conditition) { }
+let addTask = await inquirer.prompt([
+    {
+        name: `todos`,
+        type: `input`,
+        message: `what you  want to add in your todos?`
+    },
+    {
+        name: `add morwe`,
+        type: `confirm`,
+        message: `do you want to add more?`,
+        default: "false"
+    }
+]);
+todos.push(addTask.todos);
+conditition = addTask.addMore;
+console.log(todos);
